28eda529cca208d17aba6daf226504d9bfe2f7a4
Ivan Limarev <ivantaketo@gmail.com>
2024-10-01 13:56:36 +0600

    Config websocket

---
+++ drawning-backend/index.ts
@@ -0,0 +1,56 @@
+import express from 'express';
+import expressWs from 'express-ws';
+import cors from 'cors';
+import { WebSocket } from 'ws';
+
+interface Pixel {
+  x: number;
+  y: number;
+  color: string;
+}
+
+const app = express();
+expressWs(app);
+app.use(cors());
+
+const port = 8000;
+
+const router = express.Router();
+
+const connectedClients: WebSocket[] = [];
+let pixels: Pixel[] = [];
+
+router.ws('/draw', (ws, _req) => {
+  connectedClients.push(ws);
+  console.log('Client connected! Total clients', connectedClients.length);
+  ws.send(JSON.stringify(pixels));
+
+  ws.on('message', (message) => {
+    try {
+      const pixelData = JSON.parse(message.toString()) as Pixel;
+      pixels.push(pixelData);
+
+      connectedClients.forEach((clientWs) => {
+        if (clientWs !== ws && clientWs.readyState === WebSocket.OPEN) {
+          clientWs.send(message);
+        }
+      });
+
+
+    } catch (e) {
+      ws.send(JSON.stringify({ error: 'Invalid message!' }));
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected!');
+    const index = connectedClients.indexOf(ws);
+    connectedClients.splice(index, 1);
+  });
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port!`);
+});
